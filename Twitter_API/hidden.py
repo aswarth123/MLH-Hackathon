9d54ddd6ddee3aa0126a24bd3babb1da47e7fc01
aswarth123 <c.v.aswarth@gmail.com>
2021-01-18 13:30:53 +0530

    added an a code in python to access twitter API

---
+++ Twitter_API/hidden.py
@@ -0,0 +1,10 @@
+
+
+def oauth():
+    return {"consumer_key": "QzvKNqJ3TuxeGMKDIZClSYCyq",
+            "consumer_secret": "Qa939yb4tzMs6Hxw3RyKi48yvQlAKa0M6D6cnmKu7KnbuPIqFH",
+            "token_key": "1269136170346143744-S6eukeRoyaZU53WGnStBAVT3wgEtrd",
+            "token_secret": "ZAFkSQcb80GdZ9a5hd4u4BO5GZuw3x99lhLBuHS2EG5Jv"}
+
+
+#bearer key AAAAAAAAAAAAAAAAAAAAAAyNLwEAAAAAqYmW4YeAbVGboVscuzsKsAI33LE%3DW4sKfGvNULSkFVuhl6tg2jVpLUMRmwcVMzD52FMFzSqWrffLMs
